08e671a476c0a2b326e51aab2119a92ccb5c0071
Julia Wang <juliamwa@usc.edu>
2018-12-14 11:15:02 -0800

    Create background.js

---
+++ background.js
@@ -0,0 +1,10 @@
+{
+    "name": "Healthy Meal Ideas",
+    "version": "1.0",
+    "description": "Google Chrome extension providing healthy meal ideas and sample recipes.",
+    "background": {
+      "scripts": ["background.js"],
+      "persistent": false
+    },
+    "manifest_version": 2
+}
