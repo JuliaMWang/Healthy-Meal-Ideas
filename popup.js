1316af2d42d0c73fc02fee1eb24b15b8320fd8cb
Julia Wang <juliamwa@usc.edu>
2018-12-14 13:04:11 -0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -21,8 +21,8 @@ function find_quote() {
         "Classic Meat and Cheese Lasagna: Level: Medium, Total Time: several hours, Recipe: https://goo.gl/7Sx8RV",
         "Chicken Stir Fry with Rice Noodles: Level: Easy, Total Time: 30 min, Recipe: https://goo.gl/4nWCba",
         "Chinese-style Hot Pot: Level: Easy, Total Time: 30 min, Recipe: https://goo.gl/G9295w",
-        "Strawberry Chocolate Almond Acai Bowls: Level: Easy, Total Time: 10 min, Recipe: https://goo.gl/mrGcyW"       
-
+        "Strawberry Chocolate Almond Acai Bowls: Level: Easy, Total Time: 10 min, Recipe: https://goo.gl/mrGcyW",
+        "Italian Quinoa Salad with White Beans: Level: Easy, Total Time: 10 min, Recipe: https://goo.gl/HVK2AP"
     ];
 
     var random_element = message[Math.floor(Math.random() * message.length)];
