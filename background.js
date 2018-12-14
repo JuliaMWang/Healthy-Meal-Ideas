75728fd4bb9f29d21c7594aeae174388ed046eff
Julia Wang <juliamwa@usc.edu>
2018-12-14 11:18:27 -0800

    Update background.js

--- background.js
+++ background.js
@@ -4,7 +4,12 @@
     "description": "Google Chrome extension providing healthy meal ideas and sample recipes.",
     "background": {
       "scripts": ["background.js"],
-      "persistent": false
+      "persistent": false,
+      chrome.runtime.onInstalled.addListener(function() {
+        chrome.storage.sync.set({color: '#3aa757'}, function() {
+            console.log("The color is green.");
+        });
+      });
     },
     "manifest_version": 2
 }
