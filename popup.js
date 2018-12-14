09993f0a5f10841c7a6c4c11aadafdfeef7edc76
Julia Wang <juliamwa@usc.edu>
2018-12-14 13:04:47 -0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -22,7 +22,8 @@ function find_quote() {
         "Chicken Stir Fry with Rice Noodles: Level: Easy, Total Time: 30 min, Recipe: https://goo.gl/4nWCba",
         "Chinese-style Hot Pot: Level: Easy, Total Time: 30 min, Recipe: https://goo.gl/G9295w",
         "Strawberry Chocolate Almond Acai Bowls: Level: Easy, Total Time: 10 min, Recipe: https://goo.gl/mrGcyW",
-        "Italian Quinoa Salad with White Beans: Level: Easy, Total Time: 10 min, Recipe: https://goo.gl/HVK2AP"
+        "Italian Quinoa Salad with White Beans: Level: Easy, Total Time: 10 min, Recipe: https://goo.gl/HVK2AP",
+        "One-pot Zucchini Noodle Miso Soup: Level: Easy, Total Time: 10 min, Recipe: https://goo.gl/DgNq69"
     ];
 
     var random_element = message[Math.floor(Math.random() * message.length)];
