3ffa5e0d0d5284d0e42fb743e5cb8142e95d27f6
Julia Wang <juliamwa@usc.edu>
2018-12-14 13:16:21 -0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -6,6 +6,7 @@ function find_quote() {
         "Shrimp and Pasta Scampi: Level: Easy, Total Time: 30 min, Recipe: https://goo.gl/idkQQ6",
         "Baked Orange Chicken and Brown Rice: Level: Easy, Total Time: 2 hours, Recipe: https://goo.gl/doqdn9",
         "Soy Ginger Salmon with Vegetables: Level: Easy, Total Time: 10 min, Recipe: https://goo.gl/Ja2q2F",
+        "Thai Chicken Lettuce Wraps: Level: Easy, Total Time: 15 min, Recipe: https://goo.gl/mBY97A",
         "Cauliflower Fried Rice: Level: Easy, Total Time: 10 min, Recipe: https://goo.gl/FKijQ4",
         "Honey Garlic Shrimp and Broccoli: Level: Easy, Total Time: 10 min, Recipe: https://goo.gl/zeaM1e",
         "Roasted Chicken and Veggies: Level: Easy, Total Time: 15 min, Recipe: https://goo.gl/wVZikQ",
