7af98e62a70bbcc1f7b9038e815e67436302c526
Julia Wang <juliamwa@usc.edu>
2018-12-14 12:41:35 -0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -5,7 +5,7 @@ function find_quote() {
     var message = [
         "Shrimp and Pasta Scampi: Level: Easy, Total Time: 30 min, Recipe: https://goo.gl/idkQQ6",
         "Baked Orange Chicken and Brown Rice: Level: Easy, Total Time: 2 hours, Recipe: https://goo.gl/doqdn9",
-        
+        "Soy Ginger Salmon with Vegetables: Level: Easy, Total Time: 10 min, Recipe: https://goo.gl/Ja2q2F"
     ];
 
     var random_element = message[Math.floor(Math.random() * message.length)];
